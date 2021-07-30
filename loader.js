a21c579b1f5e1c1c78aea7a9e0b0a3f51200c529
Steve Barnett <mooped@gmail.com>
2021-07-30 21:22:29 +0100

    Support converting multiple files.

--- loader.js
+++ loader.js
@@ -1,22 +1,34 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import * as fs from 'fs'
-
-const content = fs.readFileSync( 'd30_dsn.gltf' )
-
-const loader = new GLTFLoader
-
 import * as atob from 'atob'
 
-global.atob = atob.default
+const dice_models = [
+  {
+    in: 'models/d30_dsn.gltf',
+    out: 'output/d30_dsn.json'
+  }
+]
 
-loader.parse( trimBuffer( content ), '', ( gltf ) => {
-  //console.log( gltf.scene.children[0].geometry.toJSON() )
-  fs.writeFileSync( 'd30_dsn.json', JSON.stringify( gltf.scene.children[0].geometry.toJSON(), null, '\t' ) )
-},
-( e) => {
-  console.error( e )
-})
+for (var options of dice_models) {
+  console.log(`${options.in} => ${options.out}`)
+  convert_model( options )
+}
+
+function convert_model ( options ) {
+  const content = fs.readFileSync( options.in )
+  
+  const loader = new GLTFLoader
+  
+  global.atob = atob.default
+  
+  loader.parse( trimBuffer( content ), '', ( gltf ) => {
+    fs.writeFileSync( options.out, JSON.stringify( gltf.scene.children[0].geometry.toJSON(), null, '\t' ) )
+  },
+  ( e) => {
+    console.error( e )
+  })
+}
 
 function trimBuffer ( buffer ) {
   const { byteOffset, byteLength } = buffer;
